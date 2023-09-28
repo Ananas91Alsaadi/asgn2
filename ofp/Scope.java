a7eb71583f9f740ffb39179aa33b89d042b9326f
devika raj <124992551+devikamehek@users.noreply.github.com>
2023-09-28 12:27:00 +0200

    Add files via upload

--- ofp/Scope.java
+++ ofp/Scope.java
@@ -1,6 +1,21 @@
 package ofp;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class Scope {
+private Scope enclosingScope; // null if global (outermost) scope
+private Map<String, Symbol> symbols = new LinkedHashMap<String, Symbol>();
+public Scope(Scope enclosingScope) { ... }
+public Scope getEnclosingScope() { ...}
+public void define(Symbol sym) { symbols.put(sym.getName(), sym); }
+// If a name cannot be resolved in this scope,
+// try enclosing/parent scope recursively.
+// If null is returned, name/identifier not found in the symbol table!
+public Symbol resolve(String name) { ... }
+// Print the contents of this scope
+@Override
+public String toString() { ... }
 
 
 
